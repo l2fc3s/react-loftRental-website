1b96097a1ed36c991a8b040ff66b761ca5de7ff2
Larry LaMar <lmrlrry@gmail.com>
2021-05-25 19:00:45 -0500

    started gallery

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,12 +3,14 @@ import "../../App.css";
 import Landing from "./Landing";
 import About from "../pages/About/About";
 import { aboutObjOne } from "./Data";
+import Gallery from "../pages/Gallery/Gallery";
 
 function Home() {
   return (
     <>
       <Landing />
       <About {...aboutObjOne} />
+      <Gallery />
     </>
   );
 }
