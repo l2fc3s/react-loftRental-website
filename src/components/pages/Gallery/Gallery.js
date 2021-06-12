936ccfb028bb6fa8217fe5a44f03aae4e22d351a
Larry LaMar <lmrlrry@gmail.com>
2021-06-11 19:40:11 -0500

    added slider

--- src/components/pages/Gallery/Gallery.js
+++ src/components/pages/Gallery/Gallery.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import ImageSlider from "../../ImageSlider";
+import { SliderData } from "../../SliderData";
 import "./Gallery.css";
 
 function Gallery() {
-  return <div></div>;
+  return (
+    <>
+      <div className="gallery-container">
+        <ImageSlider slides={SliderData} />
+      </div>
+    </>
+  );
 }
 
 export default Gallery;
