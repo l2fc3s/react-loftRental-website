b1a8397bba6f182e0919f048370f5964783ff1ff
Larry LaMar <lmrlrry@gmail.com>
2021-07-07 22:18:03 -0500

    applied changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router basename="https://l2fc3s.github.io/react-loftRental-website">
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
