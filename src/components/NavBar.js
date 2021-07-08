c0d6a701c389a4f7501b7385a10b00c871227dd8
Larry LaMar <lmrlrry@gmail.com>
2021-07-07 21:25:56 -0500

    applied minor changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import "./NavBar.css";
-import { Link } from "react-router-dom";
 import { Button } from "./Button";
 import { HashLink } from "react-router-hash-link";
 
--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./Landing.css";
 import { HashLink } from "react-router-hash-link";
 
